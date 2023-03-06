f6c55adba9a69a9fb9307222dcabd98e25892333
Decagon <decagon@Michaels-Babalola-MacBook-Air.local>
2023-03-06 02:57:29 +0100

    new solution added moveZeros

--- 11.moveZeros.js
+++ 11.moveZeros.js
@@ -7,22 +7,40 @@ input [0, 0,4] -> [4,0,0]
 
 // Brute force Approach
 function moveAllZeros(arr){
-  let newArr = [];
-  let count = 0;
+  // let newArr = [];
+  // let count = 0;
+  // for(let char of arr){
+  //   if(char !== 0){
+  //       newArr.push(char)
+  //   }else if(char === 0){
+  //       count++
+  //   }  
+  // }
+
+  // for(i=0; i<count; i++){
+  //   newArr.push(0)
+  // }
+  // return newArr
+  
+  //Solution 2(optimized)
+
+  let count = 0
   for(let char of arr){
     if(char !== 0){
-        newArr.push(char)
-    }else if(char === 0){
-        count++
-    }  
+     arr[count++] = char
+    }
+  } 
+
+  for(let i = count; i < arr.length; i++){
+    arr[i] = 0
   }
 
-  for(i=0; i<count; i++){
-    newArr.push(0)
-  }
-  return newArr
-
+  return arr
 }
+
+//Time Complexity - 0(n) - linear time even if we have multiple for loops 
+// they are not nested 0(n) + 0(n) = 0(n)
+// Space complexity - 0(1) - no extra space created
 console.log(moveAllZeros([0, 1]))
 console.log(moveAllZeros([1, 0, 2, 0, 4, 0]))
 console.log(moveAllZeros([0, 0, 4]))
