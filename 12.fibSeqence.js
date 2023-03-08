b2846a3f2daa775276f5103b1561166cf709c887
Decagon <decagon@Michaels-Babalola-MacBook-Air.local>
2023-03-08 17:59:37 +0100

    fibonacci recursion

--- 12.fibSeqence.js
+++ 12.fibSeqence.js
@@ -8,15 +8,24 @@
      fibonacci(3) = [0,1,1];
      fibonacci(7) = [0,1,1,2,3,5,8]
  */
+// Solution 1
+// function fibonacci(n) {
+//   let fib = [0, 1];
+//   for (let i = 2; i < n; i++) {
+//     fib[i] = fib[i - 1] + fib[i - 2];
+//   }
+//   return fib;
+// }
 
+//Solution 2 - Recursion
 function fibonacci(n) {
-  let fib = [0, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
-  return fib;
+ if(n < 2){
+  return n
+ }
+ return fibonacci(n -1) + fibonacci(n-2); 
+  // this will give you the nth element not the first nth elements like the first solution if you want the nth element for the first solution return fib.length-1;
 }
 
 console.log(fibonacci(2));
 console.log(fibonacci(3));
-console.log(fibonacci(7));
+console.log(fibonacci(6));
